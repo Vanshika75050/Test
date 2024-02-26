1af3b557af8123cf03920244cf8cd3fdd3dd5f15
vanshika7894 <vanshika75050@gmail.com>
2024-02-26 03:56:22 +0000

    test

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const path = require('path')
 const stocks = require('./stocks')
+var cors = require('cors') //added cors library
 
 const app = express()
-app.use(express.static(path.join(__dirname, 'static')))
+app.use(express.static(path.join(__dirname, 'static')),cors())
 
-app.get('/stocks', async (req, res) => {
-  const stockSymbols = await stocks.getStocks()
-  res.send({ stockSymbols })
+app.get('/stocks', async (req, res, next) => {
+  try {
+    const stockSymbols = [];
+    if(stockSymbols.length == 0){
+      res.send({error:'Data not found! Please check the stocks data file.'})
+    } else {
+      res.send({ stockSymbols })
+    }
+  } catch(err) {
+    next(err)
+  }
 })
 
 app.get('/stocks/:symbol', async (req, res) => {
